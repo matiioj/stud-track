b1149037bbb1e6baa1362e56598331addd9b287d
matias <matiiojj@gmail.com>
2023-11-05 23:06:54 -0300

    add profile and correct routing

--- src/app/components/ListStudents.jsx
+++ src/app/components/ListStudents.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import dayjs from 'dayjs';
+import Link from 'next/link';
 
 export function formatDate(dateString, format = 'YYYY-MM-DD') {
   const date = dayjs(dateString);
@@ -26,17 +27,16 @@ const ListStudents = () => {
   }, []);
 
   const handleUpdate = (studentId) => {
-    
+    <Link href={`/students/${studentId}`}></Link>
   };
 
   const handleSeeMore = (studentId) => {
-    console.log('See more details for student:', studentId);
-    // Implement student details page navigation here
+    <Link href={`/students/${studentId}`}></Link>
   };
 
   const handleDelete = (studentId) => {
     console.log('Delete student:', studentId);
-    // Implement student deletion logic here
+    <Link href={`/students/${studentId}`}></Link>
   };
 
   return (
@@ -44,54 +44,53 @@ const ListStudents = () => {
       <h2 className="text-2xl font-bold text-gray-800">List of Students</h2>
 
       <div className="mt-4" style={{ overflowX: 'auto' }}>
-          <table className="w-full table-auto" style={{ height: '400px' }}>
-            <thead>
-              <tr className="bg-gray-200 border-b-2 border-gray-300">
-                <th className="text-gray-700 font-bold py-2 px-4 text-center">Student ID</th>
-                <th className="text-gray-700 font-bold py-2 px-4 text-center">Name</th>
-                <th className="text-gray-700 font-bold py-2 px-4 text-center">Surname</th>
-                <th className="text-gray-700 font-bold py-2 px-4 text-center">Birthdate</th>
-                <th className="text-gray-700 font-bold py-2 px-4 text-center">Age</th>
-                <th className="text-gray-700 font-bold py-2 px-4">
-                  <div className="flex justify-center">Actions</div>
-                </th>
-              </tr>
-            </thead>
+        <table className="w-full table-auto" style={{ height: '400px' }}>
+          <thead>
+            <tr className="bg-gray-200 border-b-2 border-gray-300">
+              <th className="text-gray-700 font-bold py-2 px-4 text-center">Student ID</th>
+              <th className="text-gray-700 font-bold py-2 px-4 text-center">Name</th>
+              <th className="text-gray-700 font-bold py-2 px-4 text-center">Surname</th>
+              <th className="text-gray-700 font-bold py-2 px-4 text-center">Birthdate</th>
+              <th className="text-gray-700 font-bold py-2 px-4 text-center">Age</th>
+              <th className="text-gray-700 font-bold py-2 px-4">
+                <div className="flex justify-center">Actions</div>
+              </th>
+            </tr>
+          </thead>
 
-            <tbody>
-              {students.map((student) => (
-                <tr key={student.id} className="border-b-2 border-gray-300">
-                  <td className="text-gray-600 py-2 px-4 text-center">{student.id}</td>
-                  <td className="text-gray-600 py-2 px-4 text-center">{student.name}</td>
-                  <td className="text-gray-600 py-2 px-4 text-center">{student.surname}</td>
-                  <td className="text-gray-600 py-2 px-4 text-center">
-                    {formatDate(student.birthdate)}
-                  </td>
-                  <td className="text-gray-600 py-2 px-4 text-center">{student.age}</td>
-                  <td className="flex justify-center py-2 px-4">
-                    <button
-                      className="bg-blue-500 text-white rounded py-1 px-2 mr-2"
-                      onClick={() => handleUpdate(student.id)}
-                    >
-                      Update
-                    </button>
-                    <button
-                      className="bg-green-500 text-white rounded py-1 px-2 mr-2"
-                      onClick={() => handleSeeMore(student.id)}
-                    >
-                      See More
-                    </button>
-                    <button
-                      className="bg-red-500 text-white rounded py-1 px-2"
-                      onClick={() => handleDelete(student.id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <tbody>
+            {students.map((student) => (
+              <tr key={student.id} className="border-b-2 border-gray-300">
+                <td className="text-gray-600 py-2 px-4 text-center">{student.id}</td>
+                <td className="text-gray-600 py-2 px-4 text-center">{student.name}</td>
+                <td className="text-gray-600 py-2 px-4 text-center">{student.surname}</td>
+                <td className="text-gray-600 py-2 px-4 text-center">
+                  {formatDate(student.birthdate)}
+                </td>
+                <td className="text-gray-600 py-2 px-4 text-center">{student.age}</td>
+                <td className="flex justify-center py-2 px-4">
+                  <button
+                    className="bg-blue-500 text-white rounded py-1 px-2 mr-2"
+                    onClick={() => handleUpdate(student.id)}
+                  >
+                    Update
+                  </button>
+                  <button
+                    className="bg-green-500 text-white rounded py-1 px-2 mr-2"
+                  >
+                    <Link href={`/students/${student.id}`}>See more</Link>
+                  </button>
+                  <button
+                    className="bg-red-500 text-white rounded py-1 px-2"
+                    onClick={() => handleDelete(student.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
       <div className="flex justify-between mt-4">
--- src/app/components/StudentProfile.jsx
+++ src/app/components/StudentProfile.jsx
@@ -1,84 +1,30 @@
-import React, { useState } from 'react';
 import dayjs from 'dayjs';
-import axios from 'axios';
 
-const StudentProfile = ({ student }) => {
-  const [showMoreDetails, setShowMoreDetails] = useState(false);
-
-  const toggleMoreDetails = () => {
-    setShowMoreDetails(!showMoreDetails);
-  };
-
-  if (!student) {
-    return null;
+export function formatDate(dateString, format = 'YYYY-MM-DD') {
+    const date = dayjs(dateString);
+    if (!date.isValid()) {
+      return '';
+    }
+  
+    return date.format(format);
   }
 
-  const { id, name, surname, birthdate, age, address, phone, email } = student;
 
-  const formattedBirthdate = formatDate(birthdate);
-
-  return (
-    <div className="profile-student-card">
-      <div className="profile-student-header">
-        <h2>Student Profile</h2>
-        <p>Student ID: {id}</p>
+function StudentProfile({ ...student }) {
+    return (
+        <div key={student.id} className="flex flex-col items-center p-4 bg-white shadow-md rounded-md w-72">
+        <h1 className="text-2xl text-slate-800 font-bold">Name</h1>
+        <h2 className="text-2xl text-slate-600 pb-4">{student.name}</h2>
+        <h1 className="text-2xl text-slate-800 font-bold">Surname</h1>
+        <h2 className="text-2xl text-slate-600 pb-4">{student.surname}</h2>
+        <h1 className="text-2xl text-slate-800 font-bold">Birthdate</h1>
+        <h3 className="text-2xl text-slate-600 pb-4">{formatDate(student.birthdate)}</h3>
+        <h1 className="text-2xl text-slate-800 font-bold">Age</h1>
+        <h3 className="text-2xl text-slate-600 pb-4">{student.age}</h3>
+        
+        
       </div>
-
-      <div className="profile-student-info">
-        <div className="info-row">
-          <label>Name:</label>
-          <span>{name}</span>
-        </div>
-
-        <div className="info-row">
-          <label>Surname:</label>
-          <span>{surname}</span>
-        </div>
-
-        <div className="info-row">
-          <label>Birthdate:</label>
-          <span>{formattedBirthdate}</span>
-        </div>
-
-        <div className="info-row">
-          <label>Age:</label>
-          <span>{age}</span>
-        </div>
-
-        {showMoreDetails && (
-          <>
-            <div className="info-row">
-              <label>Address:</label>
-              <span>{address}</span>
-            </div>
-
-            <div className="info-row">
-              <label>Phone:</label>
-              <span>{phone}</span>
-            </div>
-
-            <div className="info-row">
-              <label>Email:</label>
-              <span>{email}</span>
-            </div>
-          </>
-        )}
-
-        <button onClick={toggleMoreDetails}>
-          {showMoreDetails ? 'Hide Details' : 'Show More Details'}
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export default StudentProfile;
-
-function formatDate(dateString, format = 'YYYY-MM-DD') {
-  const date = dayjs(dateString);
-  if (!date.isValid()) {
-    return '';
+    );
   }
-
-  return date.format(format);
-}
+  
+  export default StudentProfile;
